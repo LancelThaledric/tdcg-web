cda6a2c4cab0ea36db9a7b662181e0d5b5f40d69
sexysoushi <barbara1603@hotmail.com>
2013-12-15 21:22:48 +0100

    modif contact

--- ctrl/contact.php
+++ ctrl/contact.php
@@ -16,6 +16,9 @@ $£msgbox = getrender('ctrl/inc/msg.php');
 $£header = getrender('ctrl/inc/header.php');
 $£sidebar = getrender('ctrl/inc/sidebar.php');
 
+$£img_us = genlink('/resource/The%20DuskCat2.jpg');
+$£img_bab = genlink('/resource/bab.jpg');
+$£img_val = genlink('/resource/val.jpg');
 //render
 
 require 'tpl/contact.php';
--- tpl/contact.php
+++ tpl/contact.php
@@ -19,33 +19,40 @@
                 <!-- Page content -->
                 <div class="content-section" id="content-us">
 					<h2>Qui sommes nouuuuus ?</h2>
-					
 					<div class="datebox">#posté le 27/11/2013</div>
-					<p>Etudiants en IUT Informatique Imagerie Numerique.</br>
-					
-					</p>
-					.</br>
-					.</br>
-					
-					
+					<div class="fromagefondu center">
+						<strong>Etudiants en IUT Informatique Imagerie Numerique.</strong></br>
+						<img src="<?php echo $£img_us; ?>" alt="" class="shadowed">
+						
+					</div>
 				</div>
+				
+				
+				
+				
+				
+				
                 
                 <div class="content-section" id="content-findus">
 					<h2>Retrouvez-nous</h2>
-					
 					<div class="datebox">#posté le 27/11/2013  </div>
-					<p>Voici nos site personnel :
-					
-					Thalderic
-					
-					KittyPonote
-					.</br>
-					.</br>
-					
-					
-					</p>
-					
-					
+					<div class="cell-container">
+						<div class="barbara cell center">
+						<img src="<?php echo $£img_bab; ?>" alt="" class="shadowed"></br>
+						
+						Barbara SCHIAVI</br></br>
+						
+						site internet: KittyPonote.fr</br></br>
+						</div>
+						
+						<div class="valentin cell center">
+						<img src="<?php echo $£img_val; ?>" alt="" class="shadowed"></br>
+						
+						Valentin MOUROT</br></br>
+						
+						site internet: Thaledric.fr</br></br>
+						</div>
+					</div>
 				</div>
                 <!--  /Page content -->
                 
