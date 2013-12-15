adfe78d7b388c34d092dffd515b6e776c9bb6698
Thaledric <thaledric@gmail.com>
2013-12-15 21:17:58 +0100

    Correction validation level déjà validé

--- resource/js/leveltoolbar.js
+++ resource/js/leveltoolbar.js
@@ -1,12 +1,13 @@
 function checkSolution()
 {
     var inputtoolbar = document.getElementById('inputsolution');
-    var urldebut = window.location.protocol+window.location.hostname+window.location.pathname;
+    var formtoolbar = document.getElementById('formsolution');
+    var urldebut = window.location.protocol+'//'+window.location.hostname+window.location.pathname;
     if(urldebut.charAt(urldebut.length-1) != '/'){
         urldebut += '/';
     }
     urldebut += inputtoolbar.value;
     alert(urldebut);
-    window.location.href = urldebut;
-    
+    formtoolbar.setAttribute("action",urldefault);
+    formtoolbar.submit();
 }
